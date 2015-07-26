fa76b403eac6256f4662c5248246bf90779d84d5
Corkle <corey.r.mcdaniel@live.com>
2015-07-25 17:37:18 -0700

    Updated animal questions and images.

--- js/app.js
+++ js/app.js
@@ -1,47 +1,45 @@
 var quizArray = [{
-        img: 'images/1.png',
-        question: 'What is the name of this animal?',
-        choices: ['Dog', 'Cat', 'Fish', 'Pizza', 'Bird'],
-        answerIndex: 4
-},
-    {
-        img: 'images/2.png',
-        question: 'How do you spell the number 4?',
-        choices: ['One', 'Two', 'Three', 'Four', 'Five'],
-        answerIndex: 3
-},
-    {
-        img: 'images/3.png',
-        question: 'What is my favorite color?',
-        choices: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
-        answerIndex: 2
-},
-    {
-        img: 'images/4.png',
-        question: 'Which one is no?',
-        choices: ['Yes', 'No', 'Yes', 'Yes', 'Yes'],
+        img: 'images/quokka.jpg',
+        question: 'What is the name of this cheerful looking animal found in Australia?',
+    choices: ['Wallaby', 'Quokka', 'Numbat', 'Bettong', 'Bandicoot'],
         answerIndex: 1
 },
     {
-        img: 'images/5.png',
-        question: 'What year is it?',
-        choices: ['2015', '1984', '2001', '1991', '1601'],
+        img: 'images/puffer.jpg',
+        question: 'What is the name of the lethal toxin found in almost all pufferfish?',
+        choices: ['Conotoxin', 'Curare', 'Botulinum toxin', 'Caramboxin', 'Tetrodotoxin'],
+        answerIndex: 4
+},
+    {
+        img: 'images/jaguar.jpg',
+        question: 'The name "jaguar" comes from a Native American word meaning what?',
+        choices: ['Ghost', 'Spotted', 'He who kills with one leap', 'Prowler of the forest', 'Shadow'],
+        answerIndex: 2
+},
+    {
+        img: 'images/panda.jpg',
+        question: 'A group of pandas is called what?',
+        choices: ['A School', 'Pandemonium', 'A Mob', 'A Colony', 'An Embarrassment'],
+        answerIndex: 4
+},
+    {
+        img: 'images/butterfly.jpg',
+        question: 'What do butterflies taste with?',
+        choices: ['Their feet', 'Their wings', 'A fork', 'Their eyes', 'Their antennae'],
         answerIndex: 0
-
 }
                  ,
     {
-        img: 'images/6.png',
-        question: 'What year is it next year?',
-        choices: ['2015', '1984', '2001', '1991', '1601'],
-        answerIndex: 2
-
+        img: 'images/woodchuck.jpg',
+        question: 'What are female woodchucks called?',
+        choices: ['Lady chucks', 'She-chucks', 'Two buck chucks', 'Fem-chucks', 'Chuckettes'],
+        answerIndex: 1
     }
                 ]
 
 function main() {
     var quizCtrl = new QuizCtrl();
-    
+
     quizCtrl.initializeProgressBar();
     quizCtrl.loadQuestion();
 
@@ -66,7 +64,7 @@ function main() {
         .on('click', '[data-hide]', function () {
             $('.' + $(this).attr('data-hide')).hide();
         })
-    
+
     $('#start-quiz-container')
         .on('click', '#start-quiz-btn', function () {
             $('#start-quiz-container').hide();
@@ -88,7 +86,7 @@ function QuizCtrl() {
         else
             return false;
     }
-    
+
     this.showQuizScore = function () {
         var score = currentQuiz.getQuizScore()
         $('#quiz-submit-button').attr('disabled', 'disabled');
@@ -122,18 +120,17 @@ function QuizCtrl() {
     }
 
     this.initializeProgressBar = function () {
-    var numQuestions = currentQuiz.getQuizLength();
+        var numQuestions = currentQuiz.getQuizLength();
         $('#quiz-progress').children().remove();
-        for (i = 0; i < numQuestions;
-        i++) {
+        for (i = 0; i < numQuestions; i++) {
             $('#quiz-progress').append('<div class="progress-step"><div class="progress"><div class="progress-bar"></div></div><div class="progress-dot"><span class="glyphicon glyphicon-ok"></span><span class="glyphicon glyphicon-remove"></span></div></div>');
         }
         $('#quiz-progress .progress-step').width(100 / numQuestions + '%');
         $('#quiz-progress .progress-step').eq(0).addClass('active');
-        }
+    }
 
     this.updateProgress = function () {
-        var num = currentQuiz.CurrentQuestion();        
+        var num = currentQuiz.CurrentQuestion();
         if (num > 0) {
             $('#quiz-progress .progress-step').eq(num - 1).removeClass('active');
             $('#quiz-progress .progress-step').eq(num - 1).addClass('complete');
